5011dd35c8c66db89d598a11943d2f763c0d2b02
Gordonshik <51483246+Gordonshik@users.noreply.github.com>
2021-11-11 20:11:17 +0300

    Pagination has been added

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 import '../../App.css'
-import { Input } from 'antd'
+import { Input, Pagination  } from 'antd'
 import MovieCard from '../../components/MovieCard'
 import pinnedImg from '../../static/pinimg.jpg'
 import { connect } from 'react-redux'
 import { addMovies, setData } from "../../redux/actions"
 import store from '../../redux/store'
 import './style.css'
+import 'antd/dist/antd.css'
 
 const Main = ({...props}) => {
   const [searchingWord, setSearchingWord] = useState('')
-  const { movies, setData } = props;
+  const [currentPage, setCurrentPage] = useState(1)
+  const { movies, setData } = props
 
   useEffect(() => {
     if(localStorage.getItem('word')) {
@@ -18,11 +20,24 @@ const Main = ({...props}) => {
     }
   })
 
+  const paginate = movies => {
+    let pages = []
+    for(let i = 0; i < movies.length; i += 3) {
+      pages.push(movies.slice(i, i + 3))
+    }
+    return pages
+  }
+
+  const paginatedMovies = paginate(movies)
+
+  const numOfPages = paginatedMovies.length
+
   const changeSearchingWord = async(word) => {
       setData(word)
       setSearchingWord(word)
       localStorage.setItem('word', word)
       localStorage.setItem('movies', movies)
+      
   }
 
   const createMovieCard = arr => arr?.map(item => 
@@ -34,7 +49,18 @@ const Main = ({...props}) => {
     link={item.show.id}/>
   )
 
+  const getCurrentPageData = () => createMovieCard(paginatedMovies[currentPage - 1])
+
+  const currentMoviesPage = getCurrentPageData()
+
+  const currentPageData = getCurrentPageData()
+
   const movieCards = createMovieCard(movies)
+
+  const changePage = page => {
+    console.log(page)
+    setCurrentPage(page)
+  }
 
   return (
     <div className="App">
@@ -44,8 +70,15 @@ const Main = ({...props}) => {
           value={searchingWord}
           onChange={(e) => changeSearchingWord(e.target.value)}
           className='main-input'/>
+          <div className='pagination-box'>
+            <Pagination 
+              current={currentPage} 
+              total={numOfPages}
+              pageSize={1}
+              onChange={changePage}/>
+          </div>
           <div className='movies-box'>
-          {movieCards}
+          {currentMoviesPage}
           </div>
       </header>
     </div>
